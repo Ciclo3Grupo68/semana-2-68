9de3350898cec71bf95c8c781db782f5e3315834
Cristian <cccastanedaa@correo.udistrital.edu.co>
2020-12-09 22:31:00 -0500

    componente Vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <top-banner></top-banner>
+  </div>
+</template>
+
+<script>
+import TopBanner from './components/TopBanner.vue'
+
+
+export default {
+  name: "App",
+  components: { 
+    TopBanner, 
+    },
+}
+</script>
+
---
+++ src/components/TopMenu.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="navbar navbar-expand-md sticky-top navbar-dark bg-dark">
+            <div class="container">
+               <a href="" class="navbar-brand">
+                Cloud Computing
+               </a>
+               <button button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#firstnavbar">
+                <span class="navbar-toggler-icon"></span>
+               </button>
+
+               <div class="navbar-collapse" id="firstnavbar">
+                    <ul class="navbar-nav ml-auto nav nav-pills">
+                        <li class="nav-item"><a class="nav-link" id="tab-1" data-toggle="tab" role="tab" href="#">Inicio</a></li>
+                        <li class="nav-item"><a class="nav-link" id="tab-2" data-toggle="tab" role="tab" href="#">Servicios</a></li>
+                        <li class="nav-item"><a class="nav-link" id="tab-3" data-toggle="tab" role="tab" href="#">Noticias</a></li>
+                        <li class="nav-item"><a class="nav-link" id="tab-4" data-toggle="tab" role="tab" href="#">Miembros</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+</template>
+
+<script>
+    export default {
+        name: "TopMenu",
+    }
+</script>
+
+<style>
+
+</style>
